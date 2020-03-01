663d3fe02732c02b8e69ca4361976cc57e24f056
Rafał Schmidt <rafalschmidt97@gmail.com>
2020-03-01 11:34:34 +0200

    feature: simplify and process weather data

--- src/weather/process.py
+++ src/weather/process.py
@@ -23,95 +23,96 @@ config.read('config.ini')
 
 processing_percentage = config.getint('APP', 'PROCESSING_PERCENTAGE')
 
-step()
+step()  # 1
 
 # Selecting data
-w_df = pd.read_csv('data/conventional_weather_stations_inmet_brazil_1961_2019_tail.csv', sep=';', dtype=str)
-w_df.rename(columns={'Estacao': 'Code', 'Data': 'Date', 'Hora': 'Hour',
-                     'Precipitacao': 'Precipitation', 'TempBulboSeco': 'DryBulbTemp',
-                     'TempBulboUmido': 'WetBulbTemp', 'TempMaxima': 'MaxTemp',
-                     'TempMinima': 'MinTemp', 'UmidadeRelativa': 'Humidity',
-                     'PressaoAtmEstacao': 'PressureStation', 'PressaoAtmMar': 'PressureSea',
-                     'DirecaoVento': 'WindDirection', 'VelocidadeVento': 'WindSpeed',
-                     'Insolacao': 'Insolation', 'Nebulosidade': 'Cloudiness',
-                     'Evaporacao Piche': 'Evaporation', 'Temp Comp Media': 'AvgCompTemp',
-                     'Umidade Relativa Media': 'AvgRelHumidity',
-                     'Velocidade do Vento Media': 'AvgWindSpeed'}, inplace=True)
+w_df = pd.read_csv('data/simplified.csv', sep=';', dtype=str)
 
-step()
+step()  # 2
 
-# Sort data by date (connect two fields)
-w_df['Date'] = w_df['Date'] + ' ' + w_df['Hour'].apply(lambda x: x[0:2] + ':' + x[2:4] + ':00')
-w_df.drop('Hour', 1, inplace=True)
+# Add extra date fields for later
 w_df['Date'] = pd.to_datetime(w_df['Date'], infer_datetime_format=True)
-w_df.sort_values(by=['Date'], ascending=True, inplace=True)
-w_df['Day'] = w_df['Date'].apply(lambda x: x.day)
-w_df['Month'] = w_df['Date'].apply(lambda x: x.month)
+w_df['Time'] = w_df['Date'].apply(lambda x: str(int(time.mktime(x.timetuple()) * 1000)))
 w_df['Year'] = w_df['Date'].apply(lambda x: x.year)
+w_df['Month'] = w_df['Date'].apply(lambda x: x.month)
+w_df['Day'] = w_df['Date'].apply(lambda x: x.day)
 w_df['Hour'] = w_df['Date'].apply(lambda x: x.hour)
-w_df['Minutes'] = w_df['Date'].apply(lambda x: x.minute)
-w_df['Date'] = w_df['Date'].apply(lambda x: str(int(time.mktime(x.timetuple()) * 1000)))
 
-step()
+step()  # 3
 
 # Resolve location
 s_df = pd.read_csv('data/weather_stations_codes.csv', sep=';', dtype=str,
                    usecols=['Código', 'Latitude', 'Longitude', 'Altitude'])
 s_df.rename(columns={'Código': 'Code'}, inplace=True)
-
 w_df[['Latitude', 'Longitude', 'Altitude']] = w_df['Code'].apply(resolve_location)
-w_df.drop('Code', 1, inplace=True)
 
-step()
+step()  # 4
 
-# Merge fields
+# Merge fields from multiple columns
 w_df['Humidity0'], w_df['Humidity12'], w_df['Humidity18'] = [0, 0, 0]
 w_df['Pressure0'], w_df['Pressure12'], w_df['Pressure18'] = [0.0, 0.0, 0.0]
 w_df['WindDirection0'], w_df['WindDirection12'], w_df['WindDirection18'] = [0, 0, 0]
 w_df['WindSpeed0'], w_df['WindSpeed12'], w_df['WindSpeed18'] = [0.0, 0.0, 0.0]
 w_df['Cloudiness0'], w_df['Cloudiness12'], w_df['Cloudiness18'] = [0.0, 0.0, 0.0]
 
-for index, morning in w_df[1:11].iterrows():
+for index, morning in w_df[:-3].iterrows():
     if morning['Hour'] != 0:
         continue
 
     midday = w_df.iloc[index + 1]
     evening = w_df.iloc[index + 2]
 
-    # Migrate values to one row
-    w_df.at[index, 'Precipitation'] = midday['Precipitation']  # todo: there is evaporation as well (but many nulls)
-    w_df.at[index, 'MinTemp'] = midday['MinTemp']
+    w_df.at[index, 'Precipitation'] = morning['Precipitation'] \
+        if pd.notnull(morning['Precipitation']) else midday['Precipitation'] \
+        if pd.notnull(midday['Precipitation']) else evening['Precipitation'] \
+        if pd.notnull(evening['Precipitation']) else -1
+    w_df.at[index, 'MinTemp'] = midday['MinTemp'] if pd.notnull(midday['MinTemp']) else -1
+    w_df.at[index, 'MaxTemp'] = morning['MaxTemp'] if pd.notnull(morning['MaxTemp']) else -1
     w_df.at[index, 'Humidity0'], w_df.at[index, 'Humidity12'], w_df.at[index, 'Humidity18'] = \
-        [morning['Humidity'], midday['Humidity'], evening['Humidity']]
+        [morning['Humidity'] if pd.notnull(morning['Humidity']) else -1,
+         midday['Humidity'] if pd.notnull(midday['Humidity']) else -1,
+         evening['Humidity'] if pd.notnull(evening['Humidity']) else -1]
     w_df.at[index, 'Pressure0'], w_df.at[index, 'Pressure12'], w_df.at[index, 'Pressure18'] = \
-        [morning['PressureStation'], midday['PressureStation'], evening['PressureStation']]
+        [morning['PressureStation'] if pd.notnull(morning['PressureStation']) else -1,
+         midday['PressureStation'] if pd.notnull(midday['PressureStation']) else -1,
+         evening['PressureStation'] if pd.notnull(evening['PressureStation']) else -1]
     w_df.at[index, 'WindDirection0'], w_df.at[index, 'WindDirection12'], w_df.at[index, 'WindDirection18'] = \
-        [morning['WindDirection'], midday['WindDirection'], evening['WindDirection']]
+        [morning['WindDirection'] if pd.notnull(morning['WindDirection']) else -1,
+         midday['WindDirection'] if pd.notnull(midday['WindDirection']) else -1,
+         evening['WindDirection'] if pd.notnull(evening['WindDirection']) else -1]
     w_df.at[index, 'WindSpeed0'], w_df.at[index, 'WindSpeed12'], w_df.at[index, 'WindSpeed18'] = \
-        [morning['WindSpeed'], midday['WindSpeed'], evening['WindSpeed']]
+        [morning['WindSpeed'] if pd.notnull(morning['WindSpeed']) else -1,
+         midday['WindSpeed'] if pd.notnull(midday['WindSpeed']) else -1,
+         evening['WindSpeed'] if pd.notnull(evening['WindSpeed']) else -1]
     w_df.at[index, 'Cloudiness0'], w_df.at[index, 'Cloudiness12'], w_df.at[index, 'Cloudiness18'] = \
-        [morning['Cloudiness'], midday['Cloudiness'], evening['Cloudiness']]
+        [morning['Cloudiness'] if pd.notnull(morning['Cloudiness']) else -1,
+         midday['Cloudiness'] if pd.notnull(midday['Cloudiness']) else -1,
+         evening['Cloudiness'] if pd.notnull(evening['Cloudiness']) else -1]
 
 w_df.drop(['DryBulbTemp', 'WetBulbTemp', 'Evaporation', 'AvgCompTemp', 'AvgRelHumidity', 'AvgWindSpeed', 'Humidity',
-           'WindDirection', 'WindSpeed', 'Cloudiness'], 1, inplace=True)
+           'PressureStation', 'PressureSea', 'WindDirection', 'WindSpeed', 'Cloudiness'], 1, inplace=True)
 
-# todo: remove midday and evening records
+step()  # 5
 
-step()
+# Remove conflicts
+w_df = w_df[(w_df['Hour'] == 0) & (w_df['Precipitation'] != -1) & (w_df['MinTemp'] != -1) & (w_df['MaxTemp'] != -1)]
+w_df.drop('Hour', 1, inplace=True)
+w_df = w_df[(w_df['Humidity0'] != -1) & (w_df['Humidity12'] != -1) & (w_df['Humidity18'] != -1)]
+w_df = w_df[(w_df['Pressure0'] != -1) & (w_df['Pressure12'] != -1) & (w_df['Pressure18'] != -1)]
+w_df = w_df[(w_df['WindDirection0'] != -1) & (w_df['WindDirection12'] != -1) & (w_df['WindDirection18'] != -1)]
+w_df = w_df[(w_df['WindSpeed0'] != -1) & (w_df['WindSpeed12'] != -1) & (w_df['WindSpeed18'] != -1)]
+w_df = w_df[(w_df['Cloudiness0'] != -1) & (w_df['Cloudiness12'] != -1) & (w_df['Cloudiness18'] != -1)]
 
-print(w_df[1:11])
+step()  # 6
 
-# Save sorted values (to see visually which fields lack values the most
+# Sort the data
+w_df.sort_values(by=['Date'], ascending=True, inplace=True)
+w_df.reset_index(drop=True, inplace=True)
 
-# # Reduce messages to one user and take only a part of the data
-# user_messages = messages[messages['fromUser.id'] == '55a7c9e08a7b72f55c3f991e']
-# user_messages = user_messages[['text', 'sent']]
-# processing_size = int(len(user_messages) * (0.01 * processing_percentage))
-# user_messages = user_messages[:processing_size]
-#
-# # Save as one message (it is heavy computation to do it every single time)
-# connected_messages = ' '.join(map(str, user_messages.text)).lower()  # lowercase to decrease amount of features
-# file = open(f'data/processed_{processing_percentage}.pickle', 'wb')
-# pickle.dump(connected_messages, file)
-# file.close()
-# w_df.to_csv(f'data/processed_{processing_percentage}.csv', index=False)
+step()  # 7
+
+# Save data
+w_df.to_csv(f'data/processed.csv', index=False)
+processing_size = int(len(w_df) * (0.01 * processing_percentage))
+w_df[-processing_size:].to_csv(f'data/processed_{processing_percentage}.csv', index=False)
+# w_df[w_df['Code'] == '82331'].to_csv(f'data/processed_small_{processing_percentage}.csv', index=False)
